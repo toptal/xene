0946c35c137b2dd7696b4924435f00ddc64a91ff
Ike Ku <dempfi@yahoo.com>
2016-11-08 20:05:34 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Bot from './bot'
+import * as Types from './types'
 import * as queries from './queries'
 import SlackAdapter from './adapters/slack'
 
-export {Bot, queries, SlackAdapter}
+export {Bot, queries, SlackAdapter, Types}
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+import Topic from './topic'
+import Command from './command'
+import {default as BotMessage} from './messages/bot'
+import {default as UserMessage} from './messages/user'
+
+export {Topic, Command, BotMessage, UserMessage}
