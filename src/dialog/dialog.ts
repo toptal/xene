d246a095137dfce6ca2e9adf2feb66e2332882f7
Ike Ku <dempfi@yahoo.com>
2017-03-03 16:37:44 +0200

    Simplify delcarations (#6)

--- src/dialog/dialog.ts
+++ src/dialog/dialog.ts
@@ -2,6 +2,12 @@ import Bot from '../lib/bot'
 import Queue from './queue'
 import { isNil, isString, isFunction, isPlainObject } from 'lodash'
 
+export type ErrorFunc<T> = (reply: string, parsed: T) => void
+export type ParseFunc<T> = (message: string) => T
+export type CheckFunc<T> = (parsed: T) => boolean
+export type ParserObject<T> = { parse: ParseFunc<T>, check: CheckFunc<T> }
+export type Parser<T> = ParseFunc<T> | ParserObject<T>
+
 class Dialog<B extends Bot<any, {id: string}>> {
   static isDefault = false
   static match(message: string): boolean { return false }
@@ -46,61 +52,38 @@ class Dialog<B extends Bot<any, {id: string}>> {
   /**
    * Queue parse for user messages
    */
-  parse<T>(parserFunc: (msg: string) => T): Promise<T>
-  parse<T>(parserFunc: (msg: string) => T, errorMessage: B['IMessage']): Promise<T>
-  parse<T>(parserFunc: (msg: string) => T, errorCallback: (reply: string, parsed: T) => void): Promise<T>
-  parse<T>(
-    parserObject: { parse: (msg: string) => T, check: (parsed: T) => boolean },
-    errorMessage: B['IMessage']): Promise<T>
-  parse<T>(
-    parserObject: { parse: (msg: string) => T, check: (parsed: T) => boolean },
-    errorCallback: (reply: string, parsed: T) => void): Promise<T>
-  parse<T>(
-    parser: ((msg: string) => T) | { parse: (msg: string) => T, check: (parsed: T) => boolean },
-    error?: B['IMessage'] | ((reply: string, parsed: T) => void)
-  ): Promise<T> {
-    let errorCallback: (reply: string, parsed: T) => void
-    if (error && (isString(error) || isPlainObject(error))) errorCallback = () => this.message(error)
-    else errorCallback = error as (reply: string, parsed: T) => void
-    if (isFunction(parser)) parser = { parse: parser, check: (parsed) => !isNil(parsed) }
+  parse<T>(parserFunc: ParseFunc<T>)
+  parse<T>(parserFunc: ParseFunc<T>, errorMessage: B['IMessage']): Promise<T>
+  parse<T>(parserFunc: ParseFunc<T>, errorCallback: ErrorFunc<T>): Promise<T>
+  parse<T>(parserObject: ParserObject<T>, errorMessage: B['IMessage']): Promise<T>
+  parse<T>(parserObject: ParserObject<T>, errorCallback: ErrorFunc<T>): Promise<T>
+  parse<T>(parser: Parser<T>, error?: B['IMessage'] | ErrorFunc<T>): Promise<T> {
 
+    if (!isNil(error) && !isFunction(error)) error = () => this.message(error)
+    if (isFunction(parser)) parser = { parse: parser, check: parsed => !isNil(parsed) }
     return new Promise((resolve, reject) => this.queue.push({
-      parser: parser as { parse: (msg: string) => T, check: (parsed: T) => boolean },
-      error: errorCallback as (reply: string, parsed: T) => void,
+      parser: parser as ParserObject<T>,
+      error: error as ErrorFunc<T>,
       done: resolve
     }))
   }
 
-  ask<T>(message: B['IMessage'], parserFunc: (msg: string) => T): Promise<T>
-  ask<T>(message: B['IMessage'], parserFunc: (msg: string) => T, errorMessage: B['IMessage']): Promise<T>
-  ask<T>(
-    message: B['IMessage'],
-    parserFunc: (msg: string) => T,
-    errorCallback: (reply: string, parsed: T) => void): Promise<T>
-  ask<T>(
-    message: B['IMessage'],
-    parserObject: { parse: (msg: string) => T, check: (parsed: T) => boolean }): Promise<T>
-  ask<T>(
-    message: B['IMessage'],
-    parserObject: { parse: (msg: string) => T, check: (parsed: T) => boolean },
-    errorMessage: B['IMessage']): Promise<T>
-  ask<T>(
-    message: B['IMessage'],
-    parserObject: { parse: (msg: string) => T, check: (parsed: T) => boolean },
-    errorCallback: (reply: string, parsed: T) => void): Promise<T>
-  async ask<T>(
-    message: B['IMessage'],
-    parser: ((msg: string) => T) | { parse: (msg: string) => T, check: (parsed: T) => boolean },
-    error?: B['IMessage'] | ((reply: string, parsed: T) => void)
-  ): Promise<T> {
+  ask<T>(message: B['IMessage'], parserFunc: ParseFunc<T>): Promise<T>
+  ask<T>(message: B['IMessage'], parserFunc: ParseFunc<T>, errorMessage: B['IMessage']): Promise<T>
+  ask<T>(message: B['IMessage'], parserFunc: ParseFunc<T>, errorCallback: ErrorFunc<T>): Promise<T>
+  ask<T>(message: B['IMessage'], parserObject: ParserObject<T>): Promise<T>
+  ask<T>(message: B['IMessage'], parserObject: ParserObject<T>, errorMessage: B['IMessage']): Promise<T>
+  ask<T>(message: B['IMessage'], parserObject: ParserObject<T>, errorCallback: ErrorFunc<T>): Promise<T>
+  async ask<T>(message: B['IMessage'], parser: Parser<T>, error?: B['IMessage'] | ErrorFunc<T>): Promise<T> {
+
     await this.message(message)
     this.queue.resetMessage()
-    if (!error) return this.parse<T>(parser as (msg: string) => T, () => this.message(message))
-    else return this.parse<T>(parser as (msg: string) => T, error as string)
+    if (!error) error = () => this.message(message)
+    return this.parse<T>(parser as ParseFunc<T>, error)
   }
 
   startDialog(DialogClass: typeof Dialog, options: {[key: string]: any} = {}) {
-    this.bot.startDialog(DialogClass, this.chat, this.user.id, options)
+    return this.bot.startDialog(DialogClass, this.chat, this.user.id, options)
   }
 }
 
