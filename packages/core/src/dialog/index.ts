7b483f48a752c38219cb5a662f251cae124f0881
Ike Ku <dempfi@yahoo.com>
2017-06-27 00:21:32 +0700

    Rely on package dev deps

--- packages/core/src/dialog/index.ts
+++ packages/core/src/dialog/index.ts
@@ -62,8 +62,8 @@ export default class Dialog<B extends Bot<any, { id: string }>> {
     if (isFunction(parser)) parser = { parse: parser, check: parsed => !isNil(parsed) }
     return new Promise((resolve, reject) => this.queue.push({
       parser: parser as ParserObject<T>,
-      error: onError,
-      done: resolve
+      done: resolve as any,
+      error: onError
     }))
   }
 
