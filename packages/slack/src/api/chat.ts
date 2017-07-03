f12bcd02f47c3c636a829cce4b95cdc0a088923f
Ike Ku <dempfi@yahoo.com>
2017-07-03 16:55:39 +0700

    Add chat.update API method

--- packages/slack/src/api/chat.ts
+++ packages/slack/src/api/chat.ts
@@ -8,10 +8,17 @@ export default class Chat extends Base {
   postMessage(channel: string, message: Message, options: MessageOptions = {}) {
     return this.request('postMessage',
       converters.snake({
-        ...messageFormat.toSlack(message),
-        ...{ channel, asUser: true },
-        ...options }))
+        channel, asUser: true, ...options,
+        ...messageFormat.toSlack(message)
+      }))
       .then(converters.camel)
+  }
+
+  update(channel: string, ts: string, message: Message) {
+    return this.request('update', converters.snake({
+      channel, asUser: true, parse: 'none',
+      ...messageFormat.toSlack(message)
+    })).then(converters.camel)
   }
 
   del(channel: string, ts: string, options: { asUser?: boolean } = {}) {
