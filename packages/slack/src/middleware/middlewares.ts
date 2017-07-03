ae283b11f8917a427542c5e95ad146fdca692878
Ike Ku <dempfi@yahoo.com>
2017-07-03 15:51:19 +0700

    Fix parser of existing payload

--- packages/slack/src/middleware/middlewares.ts
+++ packages/slack/src/middleware/middlewares.ts
@@ -10,11 +10,13 @@ import * as format from '../helpers/formatters/message'
 import { Handler, MiddlewareContext } from './types'
 
 const streamPayload = request => rawBody(request, { encoding: true }).then(qs.parse).then(i => JSON.parse(i.payload))
+const existingPayload = payload => typeof payload === 'string' ? JSON.parse(payload) : payload
 
 const middlewareContext = (payload): MiddlewareContext => {
   const action = payload.actions[0]
   const { user, team, channel, token, callbackId } = payload
-  return { user, team, channel, token, callbackId, ephemeral: undefined,
+  return {
+    user, team, channel, token, callbackId, ephemeral: undefined,
     message: format.fromSlack(payload.originalMessage),
     action: {
       value: action.type === 'button' ? action.value : action.selectedOptions[0].value,
@@ -40,9 +42,10 @@ const getResponse = async (handler: Handler, payload) => {
   if (messageChanged) response = format.toSlack(message)
   if (messageDeleted) response = { delete_original: true }
   if (ephemeralAdded) {
-    Object.assign(response, isString(ephemeral) ? { text: ephemeral } : format.toSlack(ephemeral))
+    response = isString(ephemeral) ? { text: ephemeral } : format.toSlack(ephemeral)
     response.response_type = 'ephemeral'
     response.replace_original = false
+    response.delete_original = messageDeleted
   }
 
   return response
@@ -53,7 +56,7 @@ export const koa = async (handler: Handler, ctx: Koa.Context, next) => {
   // ctx.request.body hack is to get parsed body
   // if any body parsers middleware is already used in code
   // tslint:disable
-  const payload = get(ctx, 'request.body.payload') || await streamPayload(ctx.req)
+  const payload = existingPayload(get(ctx, 'request.body.payload')) || await streamPayload(ctx.req)
   const body = await getResponse(handler, camelize(payload))
   ctx.body = body || payload.original_message
   ctx.status = 200
@@ -62,7 +65,7 @@ export const koa = async (handler: Handler, ctx: Koa.Context, next) => {
 
 export const express = async (handler: Handler, req: Express.Request, res: Express.Response, next) => {
   if (req.method.toLowerCase() !== 'post') return
-  const payload = get(req, 'body.payload') || await streamPayload(req)
+  const payload = existingPayload(get(req, 'body.payload')) || await streamPayload(req)
   const body = await getResponse(handler, camelize(payload))
   body ? res.send(body) : res.status(200).end()
   return next()
