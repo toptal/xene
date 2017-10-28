b84f19986d79baa9feb5d9c7f8e05d515e26df5a
Ike Ku <dempfi@yahoo.com>
2017-10-28 23:58:06 +0300

    Fix failing test

--- packages/slack/src/helpers/interpolate.ts
+++ packages/slack/src/helpers/interpolate.ts
@@ -1,7 +1,7 @@
 import { isPlainObject, isArray, mapValues, template as tpl, isString } from 'lodash'
 
 export const interpolate = <T>(template: T, object: object): T => {
-  if (isPlainObject(template)) return mapValues(template, v => interpolate(v, object))
+  if (isPlainObject(template)) return mapValues(template as any, v => interpolate(v, object))
   if (isArray(template)) return (template as any).map(v => interpolate(v, object))
   if (isString(template)) return tpl(template)(object) as any
   return template
