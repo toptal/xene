ed5a07ea0d63dfb0e796978b7a3035f05a37f771
Denis Usanov <denis.usanov@toptal.com>
2018-02-17 14:26:39 +0300

    Add lookupByEmail methods for users (#26)

--- packages/slack/src/api/users.ts
+++ packages/slack/src/api/users.ts
@@ -8,4 +8,7 @@ export class Users extends APIModule {
 
   list = () =>
     this.request('list').then(get<User[]>('members'))
+
+  lookupByEmail = (email: string) =>
+    this.request('lookupByEmail', { email }).then(get<User>('user'))
 }
