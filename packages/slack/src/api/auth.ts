b5d8f319a3e484bb724d2b296ecac666a1d8cc6a
Ike Ku <dempfi@yahoo.com>
2018-02-24 10:10:26 +0200

    Fix regression of rtm API

--- packages/slack/src/api/auth.ts
+++ packages/slack/src/api/auth.ts
@@ -9,6 +9,11 @@ export type Identity = {
 }
 
 export class Auth extends APIModule {
+  protected get token() {
+    if (typeof this.tokens === 'string') return this.tokens
+    return this.tokens.botToken || this.tokens.appToken
+  }
+
   test = (): Promise<Identity> => this.request('test')
 
   revoke = (test: boolean = false): Promise<{ revoked: boolean }> =>
