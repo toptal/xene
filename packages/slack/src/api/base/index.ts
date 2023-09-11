160ab5ff845f8054835b0fc6c4ffdc0d0c28584f
Mateusz Bielec <mateusz.bielec@toptal.com>
2023-09-11 12:44:15 +0200

    Send token as header

--- packages/slack/src/api/base/index.ts
+++ packages/slack/src/api/base/index.ts
@@ -6,8 +6,8 @@ import { camel, snake } from '../../helpers/case'
 const stringify = (object: object) =>
   mapValues(snake(object), v => isObject(v) ? JSON.stringify(v) : v)
 
-const URI = (ns: string, method: string, token: string) =>
-  `https://slack.com/api/${ns}.${method}?token=${token}`
+const URI = (ns: string, method: string) =>
+  `https://slack.com/api/${ns}.${method}`
 
 export abstract class APIModule {
   protected namespace: string
@@ -22,8 +22,8 @@ export abstract class APIModule {
   }
 
   protected async request(method: string, form: object = {}) {
-    const uri = URI(this.namespace, method, this.token)
-    const response = await request(uri, stringify(form))
+    const uri = URI(this.namespace, method)
+    const response = await request(uri, stringify(form), this.token)
     if (!response.ok) throw new APIError(response.error)
     return camel(response)
   }
