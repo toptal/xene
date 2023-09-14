5863255fe5ea24fac7c21d188e7fa8919259265d
Mateusz Bielec <mateusz.bielec@toptal.com>
2023-09-14 16:24:10 +0200

    Update devDependencies

--- packages/core/src/test/resolvable.ts
+++ packages/core/src/test/resolvable.ts
@@ -1,14 +1,16 @@
 import test from 'ava'
 import { Resolvable } from '../resolvable'
 
-test.cb('Resolves with correct value', t => {
+test('Resolves with correct value', async t => {
   const s = new Resolvable<any>()
-  s.promise.then(v => t.is(v, 5)).then(t.end)
+  s.promise.then(v => t.is(v, 5))
   s.resolve(5)
+  t.pass()
 })
 
-test.cb('Rejects with correct error', t => {
+test('Rejects with correct error', async t => {
   const s = new Resolvable<any>()
-  s.promise.catch(err => t.is(err.message, 'Error5')).then(t.end)
+  s.promise.catch(err => t.is(err.message, 'Error5'))
   s.reject(new Error('Error5'))
+  t.pass()
 })
