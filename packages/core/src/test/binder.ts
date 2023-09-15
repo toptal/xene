6e537e6896cb14f44c86ad287ba0c5fc974e0de7
Mateusz Bielec <mateusz.bielec@toptal.com>
2023-09-15 12:42:28 +0200

    Update devDependencies (#35)

--- packages/core/src/test/binder.ts
+++ packages/core/src/test/binder.ts
@@ -1,13 +1,15 @@
-import test, { TestContext } from 'ava'
+import anyTest, { TestFn } from 'ava'
 import { TestBot } from './helpers/test-bot'
-import { Binder } from '../binder'
 
-interface IContext extends TestContext { context: { bot: TestBot } }
+interface Context {
+  bot: TestBot
+}
+const test = anyTest as TestFn<Context>
 test.beforeEach(t => { t.context.bot = new TestBot() })
 
 const msg = (message) => ({ channel: '#', message })
 
-test('Can bind with any matcher', (t: IContext) => {
+test('Can bind with any matcher', (t) => {
   t.plan(3)
   t.context.bot
     .when('string').do(() => t.pass())
@@ -19,11 +21,11 @@ test('Can bind with any matcher', (t: IContext) => {
   t.context.bot.incoming('', '', 'func')
 })
 
-test('Throws on unknown matcher', (t: IContext) => {
+test('Throws on unknown matcher', (t) => {
   t.throws(() => t.context.bot.when(45 as any).do(() => { /* noop */ }))
 })
 
-test('Can bind to any handler', (t: IContext) => {
+test('Can bind to any handler', (t) => {
   t.context.bot
     .when('do').do(({ channel }, b) => b.say(channel, 'done'))
     .when('talk').talk(d => d.say('talked'))
